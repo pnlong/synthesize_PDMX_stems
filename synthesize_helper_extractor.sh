8c487d874a46844384a59c762eddd55b163419ce
pnlong <phillipnlong@gmail.com>
2025-02-24 01:51:24 -0800

    updated tarball extractor script

--- synthesize_helper_extractor.sh
+++ synthesize_helper_extractor.sh
@@ -15,13 +15,13 @@
 set -e
 
 # get directories
-base_directory="${1}"
+base_directory=${1}
 data_directory="${base_directory}/data"
 
 # get parent directories, sort so it is cleaner
-subdirectories=($(ls "${data_directory}" | sed "s+^+${data_directory}/+"))
+subdirectories=($(ls ${data_directory} | sed "s+^+${data_directory}/+"))
 IFS=$'\n' # https://stackoverflow.com/questions/7442417/how-to-sort-an-array-in-bash
-subdirectories=($(sort <<<"${subdirectories[*]}")) # sort parent directories so it is cleaner
+subdirectories=($(sort <<<${subdirectories[*]})) # sort parent directories so it is cleaner
 unset IFS # unset IFS
 
 # separator line function
@@ -47,35 +47,39 @@ for i in "${!subdirectories[@]}"; do
     separator_line
     
     # get directory
-    subdirectory="${subdirectories[i]}"
-    cd "${subdirectory}" # change directory to parent directory
+    subdirectory=${subdirectories[i]}
+    cd ${subdirectory} # change directory to parent directory
 
     # get list of child tar files
     tarballs=($(find . -name "*.tar"))
 
     # output information about number of tarballs in subdirectory
-    subdirectory_name=($(basename "${subdirectory}"))
+    subdirectory_name=($(basename ${subdirectory}))
     n_tarballs=(${#tarballs[@]})
     nth_subdirectory=$((i + 1))
     printf "${subdirectory_name} (${nth_subdirectory}/${n_subdirectories}): ${n_tarballs} tarballs to extract.\n"
 
     # skip further calculations if there are no tarballs in a directory
-    if [ "${n_tarballs}" -eq 0 ]; then
+    if [ ${n_tarballs} -eq 0 ]; then
         continue
     fi
 
     # iterate through tarballs
-    for tarball in "${tarballs[@]}"; do
+    for tarball in ${tarballs[@]}; do
 
         # get variables
-        tarball=($(basename "${tarball}")) # get just basename of tarball
+        tarball=($(basename ${tarball})) # get just basename of tarball
 
         # extract tarball
-        tarball_extracted_dir=($(basename "${tarball}" ".tar")) # relative path to resulting extracted directory
-        if ! test -f "${tarball_extracted_dir}"; then # check that resulting extracted directory doesn't exist already (avoid redundant extractions)
-            # tar -xf "${tarball}" # for normal tarballs
-            # tar -xfz "${tarball}" # for gzipped tarballs
-            sleep .2
+        tarball_extracted_dir=($(basename ${tarball} ".tar")) # relative path to resulting extracted directory, removing file extension
+        if [ ! -d ${tarball_extracted_dir} ]; then # check that resulting extracted directory doesn't exist already (avoid redundant extractions)
+            if [[ ${tarball} == *.tar ]]; then # for normal tarballs
+                # tar -xf "${tarball}"
+                sleep .2
+            else # for gzipped tarballs
+                # tar -xfz "${tarball}"
+                sleep .2
+            fi
         fi
 
         # print a period when this tarball is successfully extracted
